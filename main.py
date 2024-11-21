0617b9a047ccd9fb00461be2d135988fdb1b1c56
Joshua Oloton <olotonjoshua@gmail.com>
2024-11-21 17:35:39 +0100

    feat: add cors middleware

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, Request, status
 from fastapi.exceptions import RequestValidationError
 from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
 
 from api.db.database import engine
 from api.db.models import Base
@@ -13,6 +14,22 @@ import uvicorn
 app = FastAPI()
 
 app.include_router(api_version_one)
+
+# Define the origins that you want to allow
+origins = [
+    "http://localhost",  # Allow local development
+    "http://localhost:8000",  # Allow the frontend app running on the same port
+    "https://yourfrontenddomain.com",  # Allow a specific domain (e.g., for production)
+]
+
+# Add the middleware to your FastAPI app
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get('/')
 def index():
